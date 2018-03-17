8bd4ba26c989b297cead88bbe4e71027319d76b8
JulioJu <juanes0890@gmail.com>
2018-03-17 13:53:24 +0100

    v14.4.1 with _entity.service.ts patched

--- src/main/webapp/app/entities/bank-account/bank-account.service.ts
+++ src/main/webapp/app/entities/bank-account/bank-account.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+
 import { SERVER_API_URL } from '../../app.constants';
-
-import { BankAccount } from './bank-account.model';
 import { createRequestOption } from '../../shared';
+import { BankAccount } from './bank-account.model';
 
-export type EntityResponseType = HttpResponse<BankAccount>;
+type EntityResponseType = HttpResponse<BankAccount>;
+type EntityArrayResponseType = HttpResponse<BankAccount[]>;
 
 @Injectable()
 export class BankAccountService {
@@ -16,59 +17,35 @@ export class BankAccountService {
     constructor(private http: HttpClient) { }
 
     create(bankAccount: BankAccount): Observable<EntityResponseType> {
-        const copy = this.convert(bankAccount);
-        return this.http.post<BankAccount>(this.resourceUrl, copy, { observe: 'response' })
-            .map((res: EntityResponseType) => this.convertResponse(res));
+            return this.http.post<BankAccount>(this.resourceUrl,
+bankAccount,
+                    { observe: 'response' })
+;
     }
 
     update(bankAccount: BankAccount): Observable<EntityResponseType> {
-        const copy = this.convert(bankAccount);
-        return this.http.put<BankAccount>(this.resourceUrl, copy, { observe: 'response' })
-            .map((res: EntityResponseType) => this.convertResponse(res));
+            return this.http.put<BankAccount>(this.resourceUrl,
+bankAccount,
+                    { observe: 'response' })
+;
     }
 
     find(id: number): Observable<EntityResponseType> {
         return this.http.get<BankAccount>(`${this.resourceUrl}/${id}`, { observe: 'response'})
-            .map((res: EntityResponseType) => this.convertResponse(res));
+;
     }
 
-    query(req?: any): Observable<HttpResponse<BankAccount[]>> {
+    query(req?: any): Observable<EntityArrayResponseType> {
         const options = createRequestOption(req);
         return this.http.get<BankAccount[]>(this.resourceUrl, { params: options, observe: 'response' })
-            .map((res: HttpResponse<BankAccount[]>) => this.convertArrayResponse(res));
+;
     }
 
     delete(id: number): Observable<HttpResponse<any>> {
         return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response'});
     }
 
-    private convertResponse(res: EntityResponseType): EntityResponseType {
-        const body: BankAccount = this.convertItemFromServer(res.body);
-        return res.clone({body});
-    }
 
-    private convertArrayResponse(res: HttpResponse<BankAccount[]>): HttpResponse<BankAccount[]> {
-        const jsonResponse: BankAccount[] = res.body;
-        const body: BankAccount[] = [];
-        for (let i = 0; i < jsonResponse.length; i++) {
-            body.push(this.convertItemFromServer(jsonResponse[i]));
-        }
-        return res.clone({body});
-    }
 
-    /**
-     * Convert a returned JSON object to BankAccount.
-     */
-    private convertItemFromServer(bankAccount: BankAccount): BankAccount {
-        const copy: BankAccount = Object.assign({}, bankAccount);
-        return copy;
-    }
 
-    /**
-     * Convert a BankAccount to a JSON which can be sent to the server.
-     */
-    private convert(bankAccount: BankAccount): BankAccount {
-        const copy: BankAccount = Object.assign({}, bankAccount);
-        return copy;
-    }
 }
--- src/main/webapp/app/entities/operation/operation.service.ts
+++ src/main/webapp/app/entities/operation/operation.service.ts
@@ -1,80 +1,70 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import * as moment from 'moment';
+
 import { SERVER_API_URL } from '../../app.constants';
-
-import { JhiDateUtils } from 'ng-jhipster';
-
-import { Operation } from './operation.model';
 import { createRequestOption } from '../../shared';
+import { Operation } from './operation.model';
 
-export type EntityResponseType = HttpResponse<Operation>;
+type EntityResponseType = HttpResponse<Operation>;
+type EntityArrayResponseType = HttpResponse<Operation[]>;
 
 @Injectable()
 export class OperationService {
 
     private resourceUrl =  SERVER_API_URL + 'api/operations';
 
-    constructor(private http: HttpClient, private dateUtils: JhiDateUtils) { }
+    constructor(private http: HttpClient) { }
 
     create(operation: Operation): Observable<EntityResponseType> {
-        const copy = this.convert(operation);
-        return this.http.post<Operation>(this.resourceUrl, copy, { observe: 'response' })
-            .map((res: EntityResponseType) => this.convertResponse(res));
+        const copy = this.convertDateFromClient(operation);
+            return this.http.post<Operation>(this.resourceUrl,
+copy,
+                    { observe: 'response' })
+.map((res: EntityResponseType) => this.convertDateFromServer(res));
     }
 
     update(operation: Operation): Observable<EntityResponseType> {
-        const copy = this.convert(operation);
-        return this.http.put<Operation>(this.resourceUrl, copy, { observe: 'response' })
-            .map((res: EntityResponseType) => this.convertResponse(res));
+        const copy = this.convertDateFromClient(operation);
+            return this.http.put<Operation>(this.resourceUrl,
+copy,
+                    { observe: 'response' })
+.map((res: EntityResponseType) => this.convertDateFromServer(res));
     }
 
     find(id: number): Observable<EntityResponseType> {
         return this.http.get<Operation>(`${this.resourceUrl}/${id}`, { observe: 'response'})
-            .map((res: EntityResponseType) => this.convertResponse(res));
+.map((res: EntityResponseType) => this.convertDateFromServer(res));
     }
 
-    query(req?: any): Observable<HttpResponse<Operation[]>> {
+    query(req?: any): Observable<EntityArrayResponseType> {
         const options = createRequestOption(req);
         return this.http.get<Operation[]>(this.resourceUrl, { params: options, observe: 'response' })
-            .map((res: HttpResponse<Operation[]>) => this.convertArrayResponse(res));
+.map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res));
     }
 
     delete(id: number): Observable<HttpResponse<any>> {
         return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response'});
     }
 
-    private convertResponse(res: EntityResponseType): EntityResponseType {
-        const body: Operation = this.convertItemFromServer(res.body);
-        return res.clone({body});
+
+    private convertDateFromClient(operation: Operation): Operation {
+        const copy: Operation = Object.assign({}, operation, {
+        date: operation.date != null && operation.date.isValid() ? operation.date.toJSON() : null,
+        });
+        return copy;
     }
 
-    private convertArrayResponse(res: HttpResponse<Operation[]>): HttpResponse<Operation[]> {
-        const jsonResponse: Operation[] = res.body;
-        const body: Operation[] = [];
-        for (let i = 0; i < jsonResponse.length; i++) {
-            body.push(this.convertItemFromServer(jsonResponse[i]));
+    private convertDateFromServer(res: EntityResponseType): EntityResponseType {
+        res.body.date = res.body.date != null ? moment(res.body.date) : res.body.date;
+        return res;
+    }
+
+    private convertDateArrayFromServer(res: EntityArrayResponseType): EntityArrayResponseType {
+        for (let i = 0; i < res.body.length; i++) {
+            res.body[i].date = res.body[i].date != null ? moment(res.body[i].date) : res.body[i].date;
         }
-        return res.clone({body});
-    }
-
-    /**
-     * Convert a returned JSON object to Operation.
-     */
-    private convertItemFromServer(operation: Operation): Operation {
-        const copy: Operation = Object.assign({}, operation);
-        copy.date = this.dateUtils
-            .convertDateTimeFromServer(operation.date);
-        return copy;
-    }
-
-    /**
-     * Convert a Operation to a JSON which can be sent to the server.
-     */
-    private convert(operation: Operation): Operation {
-        const copy: Operation = Object.assign({}, operation);
-
-        copy.date = this.dateUtils.toDate(operation.date);
-        return copy;
+        return res;
     }
 }
